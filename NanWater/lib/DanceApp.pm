8c0a5fe8ceb77ff3527d7561b5e4a88d4e60a888
Leon Wright <leon.wright@imdexlimited.com>
2012-12-22 09:17:47 +0800

    I'm an idiot - it wasn't working cause this shell is my work machine...

--- NanWater/lib/DanceApp.pm
+++ NanWater/lib/DanceApp.pm
@@ -17,12 +17,12 @@ get '/stations' => sub {
   set layout => 'station_control';
 
   my @stations = ( { id => '10001', name => 'Station 1', state => 'off', },
-                   { id => '10002', name => 'Station 42', state => 'on', },
+                   { id => '10002', name => 'Station 2', state => 'on', },
                    { id => '10003', name => 'Station 3', state => 'off', },
                    );
 
   my $category = 'Water Stations';
-  template 'stations-mobile', {
+  template 'stations', {
         stations => \@stations,
         category => "Watering Stations",
         title  => "Water Station Manager - Control",
