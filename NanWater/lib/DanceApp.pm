eb3a474c7e7645c5e6833151b511d7d262b58948
Leon Wright <techman83@gmail.com>
2012-12-24 21:52:29 +0800

    layouts combined, javascript seperated

--- NanWater/lib/DanceApp.pm
+++ NanWater/lib/DanceApp.pm
@@ -14,7 +14,7 @@ post '/stations/:id' => sub {
 };
 
 get '/stations' => sub {
-  set layout => 'station_control';
+#  set layout => 'station_control';
 
   my @stations = ( { id => '10001', name => 'Station 1', state => 'off', },
                    { id => '10002', name => 'Station 2', state => 'on', },
@@ -26,6 +26,7 @@ get '/stations' => sub {
         stations => \@stations,
         category => "Watering Stations",
         title  => "Water Station Manager - Control",
+	script => "station_control.js",
   };
 };
 
