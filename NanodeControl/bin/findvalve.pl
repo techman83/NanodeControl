a235d30ae6fafd0bf192e6afdd9a70c358de3e75
Leon Wright <techman83@gmail.com>
2013-05-29 19:55:24 +0800

    Script working with interrupt handling #6

--- NanodeControl/bin/findvalve.pl
+++ NanodeControl/bin/findvalve.pl
@@ -1,13 +1,56 @@
 #!/usr/bin/perl
-use Dancer;
-use NanodeControl::ValveFinder;
-my $scheduleid = $ARGV[0];
-my $appdir = config->{appdir};
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+use NanodeControl::DBsqlite;
+use LWP::Simple qw($ua get);
+
+my $stationid = $ARGV[0];
+my $appdir = "$Bin/..";
+#my $debug = 1;
+
+# Would like to catch interrupts and handle exiting properly
+$SIG{INT} = \&interrupt;
+
+if ($debug) { use Data::Dumper; }
 
 open(PID, "> $appdir/tmp/ValveFinder.pid") || die "could not open '$appdir/ValveFinder.pid' $!";
 print PID "$$";
 close(PID); 
 
-find_valve($scheduleid);
+# We really don't care if the call fails.
+$ua->timeout(1);
 
-unlink("$appdir/tmp/ValveFinder.pid") || die "could not open '$appdir/tmp/ValveFinder.pid' $!";
+my $station =  get_station($stationid);
+
+
+if ($debug) { print Dumper($station); }
+
+while (1 == 1) {
+  if ($debug) { print "Setting $stationid low\n"; }
+  get("$station->{url}/LOW");
+
+  # Nanode devices seem to stall with anything much lower than 50ms
+  select(undef,undef,undef, 0.05);
+
+  if ($debug) { print "Setting $stationid high\n"; }
+  get("$station->{url}/HIGH");
+  select(undef,undef,undef, 0.05);
+}
+
+sub interrupt {
+  my($signal)=@_;
+  if ($debug) { print "Caught Interrupt\: $signal \n"; }
+  if ($debug) { print "Now Exiting\n"; }
+
+  if ($station->{reversed} == 1) {
+    get("$station->{url}/HIGH")
+  } else {
+    get("$station->{url}/LOW");
+  }
+  
+  unlink("$appdir/tmp/ValveFinder.pid") || die "could not open '$appdir/tmp/ValveFinder.pid' $!";
+     
+  exit(1);
+}
+
+__END__
