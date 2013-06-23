c83dbf8cbe6ce1e44c6ff23c624c8254d89c364c
Leon Wright <techman83@gmail.com>
2013-06-23 14:38:40 +0800

    This is functional enough for now, will replace with HiPi at some point - closes #21

--- NanodeControl/lib/NanodeControl/PIcontrol.pm
+++ NanodeControl/lib/NanodeControl/PIcontrol.pm
@@ -6,16 +6,28 @@ our @EXPORT = qw(get_pigpio_state set_pigpio_state);
 
 sub get_pigpio_state {
   my ($gpio) = @_;
+
   my $result = `/usr/local/bin/gpio -g read $gpio`; # There are better more reliable ways to do this. Will implement later.
-  $result=~s/\s+$//;
-  if ($result == 1) { $result = 255; }
+  if ($result == 0) {
+    $result = 'LOW';
+  } elsif ($result == 1) {
+    $result = 'HIGH';
+  } else {
+    return "failed";
+  }
+
   return $result;
 }
 
 sub set_pigpio_state {
   my ($gpio,$state) = @_;
-  # Not all Pi pins are PWM controllable, unlike the arduino.
-  if (! $state == 0 && $state < 256) { $state = 1; }
+  if ($state eq 'LOW') {
+    $state = 0;
+  } elsif ($state eq 'HIGH') {
+    $state = 1;
+  } else {
+    return "failed";
+  }
 
   # Set state
   system("/usr/local/bin/gpio -g write $gpio $state");
