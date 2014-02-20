e476ae3ef3a7a235db198aabab87fe3a85ca1aea
Leon Wright <leon.wright@imdexlimited.com>
2014-02-20 16:33:37 +0800

    moar dev

--- NanodeControl-Web/lib/NanodeControl/API.pm
+++ NanodeControl-Web/lib/NanodeControl/API.pm
@@ -9,11 +9,13 @@ get '/api/stations' => sub {
     debug("Stations Called");
     fork_call {
       debug("Forking");
+      sleep 5;
+    } sub {
       my $data->{type} = 'update';
       $data->{content} = 'content!';
       ws_send $data;
       debug("Message Sent");
-    }
+    };
     return;
 };
 
