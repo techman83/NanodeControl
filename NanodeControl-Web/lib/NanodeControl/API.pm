fecadbd1b4a98e02e41b89b6e66b4917d9c6596b
Leon Wright <techman83@gmail.com>
2014-02-23 12:25:23 +0800

    Initial Knockout dev + update/insert/retrieve working

--- NanodeControl-Web/lib/NanodeControl/API.pm
+++ NanodeControl-Web/lib/NanodeControl/API.pm
@@ -4,26 +4,58 @@ use Dancer::Plugin::WebSocket;
 use Dancer::Plugin::Mongo;
 use AnyEvent::Util;
 
+# settings
+our $db = config->{plugins}{Mongo}{db_name};
 
 get '/api/:collection' => sub {
   my $collection = params->{collection};
   debug("Getting $collection");
+  my $cursor = mongo->get_database($db)->get_collection( "$collection" )->find();
+  my $data;
+  @{$data} = $cursor->all;
+  debug(@{$data});
+  return to_json($data,{allow_blessed=>1,convert_blessed=>1});
+};
+
+post '/api/:collection' => sub {
+  my $collection = params->{collection};
+  my $data = from_json(request->body);
+
   fork_call {
-    my ($collection) = @_;
-    debug("Forking: $collection");
-    $collection = mongo->database->get_collection( "$collection" );
-    return $collection;
-  } $collection, sub {
+    my ($collection, $data) = @_;
+
+    # Get collection
+    $collection = mongo->get_database($db)->get_collection( "$collection" );
+
+    # Insert data
+    my $insert = $collection->insert($data);
+
+    # Get result
+    my $data = $collection->find_one({ _id => MongoDB::OID->new("$insert->{value}") });
+    return $data;
+  } ($collection, $data), sub {
     my ($data) = @_;
-    debug($data);
-    ws_send $data;
+    # Send data to clients
+    my $result->{type} = 'insert';
+    $result->{content} = $data;
+    $result = to_json($result,{allow_blessed=>1,convert_blessed=>1});
+    debug($result);
+    ws_send $result;
     debug("Message Sent");
   };
   return;
 };
 
-get '/api/stations' => sub {
+true;
 
+__END__
+post '/api/message' => sub {
+  my $id = from_json(request->body);
+  debug($id);
+  my $collection = mongo->get_database($db)->get_collection( "stations" );
+  my $data->{content} = $collection->find_one({ _id => MongoDB::OID->new("$id->{id}") });
+  $data->{type} = 'update';
+  ws_send to_json($data,{allow_blessed=>1,convert_blessed=>1});
   return;
 };
-true;
+
