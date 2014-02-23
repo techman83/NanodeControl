5c78ebdb8a04dadc5181a061adb41d4586caa43e
Leon Wright <techman83@gmail.com>
2014-02-23 12:25:23 +0800

    Initial Knockout dev + update/insert/retrieve working

--- NanodeControl-Web/lib/NanodeControl/Web.pm
+++ NanodeControl-Web/lib/NanodeControl/Web.pm
@@ -2,6 +2,7 @@ package NanodeControl::Web;
 use Dancer ':syntax';
 use Dancer::Plugin::WebSocket;
 use AnyEvent::Util;
+use Dancer::Plugin::DebugToolbar;
 
 our $VERSION = '0.5';
 
--- NanodeControl-Web/public/javascripts/client.js
+++ NanodeControl-Web/public/javascripts/client.js
@@ -1,18 +1,48 @@
+ko.bindingProvider.instance = new StringInterpolatingBindingProvider();
 var socket = new WebSocket(ws_path)
+
+var viewModel = {
+  stations: ko.mapping.fromJS([]),
+}
+
+ko.applyBindings(viewModel);
 
 socket.onopen = function() {
   document.getElementById('conn-status').innerHTML = 'Connected';
 };
 
-socket.onmessage = function(e) {
-  var data = JSON.parse(e.data);
-  console.log(data);
-};
+$.get( "/api/stations", function( data ) {
+})
+  .done(function(data) {
+    ko.mapping.fromJSON(data, viewModel.stations)
+    socket.onmessage = function (msg) {
+      data = JSON.parse(msg.data)
+      console.log(data);
+      if (data.msg) {
+        data = JSON.parse(data.msg)
+        console.log(data)
+        if (data.type == 'remove') {
+          viewModel.stations.remove(function(item) {
+            return item._id.$oid() == data.content
+          })
+        }
+        if (data.type == 'insert') {
+          viewModel.stations.push(ko.mapping.fromJS(data.content))
+        }
+        if (data.type == 'update') {
+          var match = ko.utils.arrayFirst(viewModel.stations(), function(item) {
+            return data.content._id.$oid === item._id.$oid();
+          });
+          if (match) {
+            viewModel.stations.splice(viewModel.stations.indexOf(match),1,ko.mapping.fromJS(data.content));
+          }
+        }
+        if (data.type == 'notify') {
+        }
+      }
+    }
+  })
 
-function get_data() {
-  $.ajax({
-    url: "/api/stations",
-    type: 'Get'
-  });
-  console.log("Get station data");
+function addStation(data) {
+
 }
