450e357edb84ebbacc63bfb7739c287a22c9ab3e
Leon Wright <leon.wright@imdexlimited.com>
2014-03-06 17:29:03 +0800

    basic on/off schedule working

--- NanodeControl-Web/bin/runschedule.pl
+++ NanodeControl-Web/bin/runschedule.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-use LWP::Simple qw(get post);
+use LWP::Simple 'get';
+use LWP::Simple::Post 'post';
 use JSON;
 use Data::Dumper;
 my $apikey = $ARGV[0];
@@ -13,50 +14,26 @@ print Dumper($schedule);
 # Turn on Master Station (Master valve in irrigation etc)
 my $masterstation;
 if ( $schedule->{master} ) {
-  post("http://localhost:3000/$schedule->{master}/HIGH");
+  print "Master $schedule->{master} ON\n";
+  post("http://localhost:3000/api/$schedule->{master}/HIGH");
 }
 
-## Run through each station schedule
-#foreach my $station (@stations) {
-#  my $duration = $station->{duration};
-#  my $station =  get_station($station->{id});
-#  print Dumper($station);
-#
-#  if ($station->{reversed} == 1) {
-#    set_station_state($station->{url},"LOW")
-#  } else {
-#    set_station_state($station->{url},"HIGH");
-#  }
-#  
-#  $duration = to_seconds($duration);
-#  print "$duration"; 
-#  sleep($duration);
-#
-#  if ($station->{reversed} == 1) {
-#    set_station_state($station->{url},"HIGH")
-#  } else {
-#    set_station_state($station->{url},"LOW");
-#  }
-#}
-#
-## Turn off Master Station
-#unless ( $schedule->{master} == 0 ) {
-#  $masterstation =  get_station($schedule->{master});
-#  
-#  if ($station->{reversed} == 1) {
-#    set_station_state($station->{url},"HIGH");
-#  } else {
-#    set_station_state($station->{url},"LOW")
-#  }
-#}
+# Run through each station schedule
+foreach my $station (@{$schedule->{scheduledStations}}) {
+  
+  print "Station $station ON\n";
+  post("http://localhost:3000/api/$station/HIGH");
 
+  print "Sleeping for $schedule->{duration}\n";
+  sleep($schedule->{duration});
 
+  print "Station $station OFF\n";
+  post("http://localhost:3000/api/$station/LOW");
+}
 
-__END__
-$VAR1 = {
-          'runorder' => 1,
-          'duration' => 500,
-          'id' => 10001,
-          'master' => 10002
-        };
-
+# Turn on Master Station (Master valve in irrigation etc)
+my $masterstation;
+if ( $schedule->{master} ) {
+  print "Master $schedule->{master} OFF\n";
+  post("http://localhost:3000/api/$schedule->{master}/LOW");
+}
